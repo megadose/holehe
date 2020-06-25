33f9963fd27713a9c74942d42b688bc1394789f7
megadose <51822495+megadose@users.noreply.github.com>
2020-06-26 01:05:56 +0200

    Create holehe.py

---
+++ holehe.py
@@ -0,0 +1,39 @@
+from holehe import *
+from tqdm import tqdm
+import argparse
+from termcolor import colored
+parser = argparse.ArgumentParser()
+parser.add_argument("-e", "--email", help="Email of the target")
+args = parser.parse_args()
+websites=[adobe,ebay,apple,facebook,instagram,tumblr,pastebin,github,twitter,pinterest,lastfm,spotify,firefox]
+
+infos =[]
+description = colored("Email used","green")+","+colored(" Email not used","magenta")+","+colored(" Rate limit","red")+"\n"
+for website in tqdm(websites):
+    infos.append({str(website).split(" ")[1].split(" ")[0]:website(args.email)})
+print("\033[H\033[J")
+print("*"*25)
+print(args.email)
+print("*"*25+"\n")
+for i in infos:
+    key, value = next(iter(i.items()))
+    i = value
+    #print(i)
+    if i["rateLimit"]==True:
+        websiteprint=colored(key,"red")
+    elif i["exists"]==False :
+        websiteprint=colored(key,"magenta")
+    else:
+        toprint=""
+        if i["emailrecovery"]!= None:
+            toprint+=" "+i["emailrecovery"]
+        if i["phoneNumber"]!= None:
+            toprint+=" / "+i["phoneNumber"]
+        if i["others"]!= None:
+            toprint+=" / FullName "+i["others"]["FullName"]
+            toprint+=" / profilePicture "+i["others"]["profilePicture"]
+
+        websiteprint=colored(str(key)+toprint,"green")
+    print(websiteprint)
+
+print("\n"+description)
