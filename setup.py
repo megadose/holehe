fc6cc51cdb129d4f7e9c991a96d9e064a487e239
megadose <51822495+megadose@users.noreply.github.com>
2020-09-17 09:40:50 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='holehe',
-    version="1.51",
+    version="1.52",
     packages=find_packages(),
     author="megadose",
     install_requires=["requests","argparse","termcolor","tqdm", "mechanize","lxml","bs4"],
-    description="holehe allows you to check if the mail is used on different sites like twitter, instagram and will retrieve information on sites with the forgotten password function.",
+    description="holehe allows you to check if the mail is used on different sites like twitter, instagram , snapchat and will retrieve information on sites with the forgotten password function.",
     include_package_data=True,
     url='http://github.com/megadose/holehe',
     entry_points = {'console_scripts': ['holehe = holehe.core:main']},
