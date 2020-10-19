216e67edb8387c8cf9836b038826e2a62e234c49
djrieger <djrieger@users.noreply.github.com>
2020-10-19 14:33:15 +0200

    Added Xing and Quizlet modules

--- holehe/core.py
+++ holehe/core.py
@@ -1570,6 +1570,54 @@ def mail_ru(email):
         return({"rateLimit": True, "exists": False, "emailrecovery": None, "phoneNumber": None, "others": None})
 
 
+def xing(email):
+    headers = {
+        'User-Agent': random.choice(ua["browsers"]["chrome"]),
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        'Accept-Language': 'en,en-US;q=0.5',
+        'DNT': '1',
+        'Connection': 'keep-alive',
+        'Upgrade-Insecure-Requests': '1',
+    }
+
+    s = requests.session()
+    response = s.get("https://www.xing.com/start/signup?registration=1", headers=headers)
+    headers['x-csrf-token'] = response.cookies["xing_csrf_token"]
+
+    data = {
+      'signup_minireg': {
+	'email': email,
+	'password': '',
+	'tandc_check': '1',
+	'signup_channel': 'minireg_fullpage',
+	'first_name': '',
+	'last_name': ''
+      }
+    }
+
+    response = s.post('https://www.xing.com/welcome/api/signup/validate', headers=headers, json=data)
+    try:
+        errors = response.json()["errors"]
+        if "signup_minireg[email]" in errors and errors["signup_minireg[email]"].startswith("We already know this e-mail address."):
+            return({"rateLimit": False, "exists": True, "emailrecovery": None, "phoneNumber": None, "others": None})
+        else:
+            return({"rateLimit": False, "exists": False, "emailrecovery": None, "phoneNumber": None, "others": None})
+    except BaseException:
+        return({"rateLimit": True, "exists": False, "emailrecovery": None, "phoneNumber": None, "others": None})
+
+
+def quizlet(email):
+    headers = {
+        'User-Agent': random.choice(ua["browsers"]["chrome"]),
+    }
+
+    response = requests.get("https://quizlet.com/webapi/3.3/validate-email", headers=headers, params={'email': email})
+
+    try:
+        existingAccount = response.json()["responses"][0]["data"]["validateEmail"]["existingAccount"]
+        return({"rateLimit": False, "exists": existingAccount != None, "emailrecovery": None, "phoneNumber": None, "others": None})
+    except BaseException:
+        return({"rateLimit": True, "exists": False, "emailrecovery": None, "phoneNumber": None, "others": None})
 
 def main():
     print('Github : https://github.com/megadose/holehe')
@@ -1620,6 +1668,7 @@ def main():
         office365,
         pinterest,
         pornhub,
+        quizlet,
         rambler,
         samsung,
         snapchat,
@@ -1631,6 +1680,7 @@ def main():
         venmo,
         vrbo,
         wordpress,
+        xing,
         yahoo]
 
     que = queue.Queue()
