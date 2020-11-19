f0de296f3f0b05e48c18d8f9e46802691401159a
megadose <megadose@protonmail.com>
2020-11-19 09:40:21 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='holehe',
-    version="1.55.3.2",
+    version="1.55.4.1",
     packages=find_packages(),
     author="megadose",
     install_requires=["requests","argparse","termcolor","tqdm", "mechanize","lxml","bs4"],
