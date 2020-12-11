5da1adaa6aa5632977ce69f3dfb2e6b54686ccda
megadose <megadose@protonmail.com>
2020-12-11 15:31:48 +0100

    Update Version

--- holehe/core.py
+++ holehe/core.py
@@ -26,7 +26,7 @@ from holehe.localuseragent import ua
 
 DEBUG = False
 
-__version__ = "1.57.10.4"
+__version__ = "1.57.10.4.1"
 if not DEBUG:
     checkVersion = httpx.get("https://pypi.org/pypi/holehe/json")
 if not DEBUG and checkVersion.json()["info"]["version"] != __version__:
@@ -92,7 +92,7 @@ async def maincore():
 
     email = ask_email()
 
-    client = httpx.AsyncClient()
+    client = httpx.AsyncClient(timeout=60)
     out = []
     async with trio.open_nursery() as nursery:
         for website in tqdm(websites):
