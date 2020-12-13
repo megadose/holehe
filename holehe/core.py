7d42e4baef7689995ca89f968b6fd6a023fc306c
megadose <megadose@protonmail.com>
2020-12-13 09:06:49 +0100

    Update Version For Fix Myspace

--- holehe/core.py
+++ holehe/core.py
@@ -26,7 +26,7 @@ from holehe.localuseragent import ua
 
 DEBUG = False
 
-__version__ = "1.57.10.4.2"
+__version__ = "1.57.10.4.3"
 if not DEBUG:
     checkVersion = httpx.get("https://pypi.org/pypi/holehe/json")
 if not DEBUG and checkVersion.json()["info"]["version"] != __version__:
