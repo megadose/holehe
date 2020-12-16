bee3436dc2f6dfa74502cd904fbda533d18879a3
megadose <megadose@protonmail.com>
2020-12-16 23:26:16 +0100

    🎉 Async version 🎉

--- holehe/core.py
+++ holehe/core.py
@@ -26,7 +26,7 @@ from holehe.localuseragent import ua
 
 DEBUG = False
 
-__version__ = "1.57.10.4.14"
+__version__ = "1.58"
 checkVersion = httpx.get("https://pypi.org/pypi/holehe/json")
 
 if not DEBUG and checkVersion.json()["info"]["version"] != __version__:
