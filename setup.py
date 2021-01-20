28a38c73cab71fbab4bda95f47e46e6aa5a2fd9b
megadose <megadose@protonmail.com>
2021-01-20 17:20:42 +0100

    Fix pinterest

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='holehe',
-    version="1.58.6.2",
+    version="1.58.6.3",
     packages=find_packages(),
     author="megadose",
     author_email="megadose@protonmail.com",
