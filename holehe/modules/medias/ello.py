01c8c79d0ed78496d440ae01b115916818b00d9b
megadose <megadose@protonmail.com>
2021-02-16 11:30:46 +0100

    Fix ello.co

--- holehe/modules/medias/ello.py
+++ holehe/modules/medias/ello.py
@@ -7,6 +7,10 @@ async def ello(email, client, out):
     domain = "ello.co"
     method= "register"
     frequent_rate_limit=False
+    def get_random_string(length):
+        letters = string.ascii_lowercase
+        result_str = ''.join(random.choice(letters) for i in range(length))
+        return(result_str)
 
     headers = {
         'User-Agent': random.choice(ua["browsers"]["firefox"]),
@@ -31,12 +35,23 @@ async def ello(email, client, out):
                         "phoneNumber": None,
                         "others": None})
         else:
-            out.append({"name": name,"domain":domain,"method":method,"frequent_rate_limit":frequent_rate_limit,
-                        "rateLimit": False,
-                        "exists": True,
-                        "emailrecovery": None,
-                        "phoneNumber": None,
-                        "others": None})
+            data2 = '{"email":"' + get_random_string(15)+"@"+email.split('@')[1] + '"}'
+            response2 = await client.post('https://ello.co/api/v2/availability', headers=headers, data=data2)
+            if response2.json()["availability"]["email"]:
+                out.append({"name": name,"domain":domain,"method":method,"frequent_rate_limit":frequent_rate_limit,
+                            "rateLimit": False,
+                            "exists": True,
+                            "emailrecovery": None,
+                            "phoneNumber": None,
+                            "others": None})
+            else:
+                out.append({"name": name,"domain":domain,"method":method,"frequent_rate_limit":frequent_rate_limit,
+                            "rateLimit": False,
+                            "exists": False,
+                            "emailrecovery": None,
+                            "phoneNumber": None,
+                            "others": None})
+
     except BaseException:
         out.append({"name": name,"domain":domain,"method":method,"frequent_rate_limit":frequent_rate_limit,
                     "rateLimit": True,
