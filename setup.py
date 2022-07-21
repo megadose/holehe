f7d151c49cafd958c3adb4d75aacca549315ce23
Palenath <megadose@protonmail.com>
2022-07-21 07:32:28 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='holehe',
-    version="1.60.3",
+    version="1.61",
     packages=find_packages(),
     author="megadose",
     author_email="megadose@protonmail.com",
